f670b19579c0d8e2abd66a686ba61fa05f339b98
Jack299q <1625621154@qq.com>
2024-09-21 21:58:54 +0800

    执行指令 单元测试

--- src/main/java/ithaic/imitate_os/process/CPU.java
+++ src/main/java/ithaic/imitate_os/process/CPU.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static java.lang.Thread.sleep;
+
 
 @Data
 @NoArgsConstructor
@@ -18,23 +20,38 @@ public class CPU {
 
     //模拟CPU运行
     public void run(){
-//        while(true){
-            checkPSW();
-            //TODO: 解析指令，执行指令
-            String[] instruction = {"X=1000","x++","x++","x--","!A100","end"};
-            for(String ins : instruction){
-                parseInstruction(ins);
+        Runnable task = ()->{
+            while(true){
+                checkPSW();
+
+
+                //TODO: 解析指令，执行指令
+
+                //以下为 测试指令
+                String[] instruction = {"X=1000","x++","x++","x--","!A100","end"};
+                for(String ins : instruction){
+                    parseInstruction(ins);
+                }
+
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-//        }
+        };
+        task.run();
     }
 
     //检查PSW 判断是否需要中断处理
     private void checkPSW(){
         if((PSW & 0b001) == 0b001){
             //TODO: 程序结束中断
-        }else if((PSW & 0b010) == 0b010){
+        }
+        if((PSW & 0b010) == 0b010){
             //TODO: 时间片结束中断
-        }else if ((PSW & 0b100) == 0b100){
+        }
+        if ((PSW & 0b100) == 0b100){
             //TODO: I/O中断
         }
     }
