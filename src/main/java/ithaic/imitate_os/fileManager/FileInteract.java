d2b26dfa0dc0530732dfd0a2ccf5bac902f8a8b8
Jack299q <1625621154@qq.com>
2024-09-26 00:11:12 +0800

    增加exec命令,可执行.e文件

--- src/main/java/ithaic/imitate_os/fileManager/FileInteract.java
+++ src/main/java/ithaic/imitate_os/fileManager/FileInteract.java
@@ -2,14 +2,17 @@ package ithaic.imitate_os.fileManager;
 
 import ithaic.imitate_os.fileManager.fileKind.Directory;
 import ithaic.imitate_os.fileManager.fileKind.MyFile;
+import ithaic.imitate_os.process.ProcessManager;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import lombok.Data;
 import lombok.Getter;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 @Data
 public class FileInteract {
@@ -108,6 +111,9 @@ public class FileInteract {
             for (int i = 0; i < 10; i++) {
                 Disk.printBlock(i);
             }
+        }
+        else if(commandArray[0].equals("exec")){  //执行可执行文件
+            FileUtils.executeFile(sourceArray.toArray(new String[0]));
         }
         else{
             FileInteract.getHistoryCommand().appendText("命令错误！\n");
